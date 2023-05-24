6149fab9d08e7cbd1a7d6c88d0bceb987a4c15f9
sunshine <79882515+allen746@users.noreply.github.com>
2023-05-24 17:28:08 +0800

    Update change_clash_proxy.py

--- change_clash_proxy.py
+++ change_clash_proxy.py
@@ -3,26 +3,36 @@ import random
 import configparser
 
 cf = configparser.ConfigParser()
-cf.read("config.ini")
+
+cf.read("config.ini",encoding='utf-8')
 port = cf.get('clash api', 'port')
 secret = cf.get('clash api', 'secret')
 selector = cf.get('clash api', 'selector')
 
 port = port
 secret = secret
+
+headers_secret = {
+        'Authorization': 'Bearer {}'.format(secret)
+    }
+
+url_all_proxies_info = 'http://localhost:{}/proxies'.format(port)
+url_all_proxies_info = requests.get(url_all_proxies_info,headers=headers_secret).json()
 url_all_proxies = 'http://localhost:{}/proxies/{}'.format(port,selector)
 
 def change_proxy(url_all_proxies):
-    headers_secret = {
-        'Authorization': 'Bearer {}'.format(secret)
-    }
     res_proxies = requests.get(url_all_proxies,headers=headers_secret).json()
     now_proxy = res_proxies['now']
     print('现在的代理是{}'.format(now_proxy))
     proxy_name_list = res_proxies['all'][3:-3]
     if now_proxy not in ['DIRECT','REJECT','账号邮箱看最新的地址','NETV2','自动选择','故障转移']:
         proxy_name_list.remove(now_proxy)
-    random_proxy = random.choice(proxy_name_list)
+
+    while True:
+        random_proxy = random.choice(proxy_name_list)
+        delay = url_all_proxies_info['proxies'][random_proxy]['history'][-1]['delay']
+        if delay!=0:
+            break
     print('随机选择的代理为{}'.format(random_proxy))
     data = {
         "name": random_proxy
@@ -31,6 +41,7 @@ def change_proxy(url_all_proxies):
         "content-type": "application/json",
         'Authorization': 'Bearer {}'.format(secret)
     }
+
     res = requests.put(url=url_all_proxies,json=data,headers=headers)
     print('切换代理请求的状态码为{}'.format(res.status_code))
     if res.status_code == 204:
